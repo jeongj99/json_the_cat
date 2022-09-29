714ae36ad14565ab04801dcc93bdc369c6a99085
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-29 19:24:38 +0000

    Refactored, so that it uses a callback function and exported it to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,20 @@
 const request = require('request');
-const breed = (process.argv)[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, (error, response, body) => {
-  if (error !== null) {
-    console.log("404: Page not found\nThe page you are looking for doesn't exist or an other error occured.");
-    process.exit();
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Breed not found");
-    process.exit();
-  }
-  console.log(data[0].description);
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, _, body) => {
+    if (error !== null) {
+      body = null;
+      callback(error, body);
+      process.exit();
+    }
+    if (body === '[]') {
+      error = 'Invalid breed name';
+      body = null;
+    } else {
+      body = JSON.parse(body)[0].description;
+    }
+    callback(error, body);
+  });
+};
+
+module.exports = { fetchBreedDescription };
